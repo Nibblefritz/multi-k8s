daeb2f9520a18396d1d5fe63f5d765dc7b290a28
Nibblefritz <kyle@fifieldonline.com>
2019-08-19 19:03:26 -0600

    adding travis and deploy.sh scripts

---
+++ deploy.sh
@@ -0,0 +1,13 @@
+docker build -t nibblefritz/multi-client:latest -t nibblefritz/multi-client:$SHA -f ./client/Dockerfile ./client
+docker build -t nibblefritz/multi-server:latest -t nibblefritz/multi-server:$SHA -f ./server/Dockerfile ./server
+docker build -t nibblefritz/mulit-worker:latest -t nibblefritz/multi-worker:$SHA -f ./worker/Dockerfile ./worker
+docker push nibblefritz/multi-client:latest
+docker push nibblefritz/multi-client:$SHA
+docker push nibblefritz/multi-server:latest
+docker push nibblefritz/multi-server:$SHA
+docker push nibblefritz/multi-worker:latest
+docker push nibblefritz/multi-worker:$SHA
+kubectl apply -f k8s
+kubectl set image deployments/server-deployment server=nibblefritz/multi-server:$SHA
+kubectl set image deployments/client-deployment client=nibblefritz/multi-client:$SHA
+kubectl set image deployments/worker-deployment worker=nibblefritz/multi-worker:$SHA
